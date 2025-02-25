8af5f7cd7a7f6ab14b1f50423cbd7e86295faa7d
Dhista Ardiansyah <56432757+gnotnek@users.noreply.github.com>
2025-02-25 15:57:54 +0700

    Add category management endpoints for retrieving, updating, and deleting categories

--- internal/category/dto.go
+++ internal/category/dto.go
@@ -3,3 +3,7 @@ package category
 type CreateCategoryRequest struct {
 	Name string `json:"name"`
 }
+
+type UpdateCategoryRequest struct {
+	Name string `json:"name"`
+}
--- internal/category/handler.go
+++ internal/category/handler.go
@@ -5,6 +5,7 @@ import (
 	"net-http-boilerplate/internal/api/resp"
 	"net-http-boilerplate/internal/entity"
 	"net/http"
+	"strconv"
 
 	"github.com/rs/zerolog/log"
 )
@@ -45,4 +46,107 @@ func (h *httpHandler) CreateCategory(w http.ResponseWriter, r *http.Request) {
 		resp.WriteError(w, err)
 		return
 	}
+}
+
+func (h *httpHandler) GetCategories(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	categories, err := h.service.FindAll(ctx)
+	if err != nil {
+		if err == ErrCategoryNotFound {
+			log.Ctx(ctx).Error().Err(err).Msg("categories not found")
+			resp.WriteJSON(w, http.StatusNotFound, map[string]string{"message": "categories not found"})
+			return
+		}
+
+		log.Ctx(ctx).Error().Err(err).Msg("failed to get categories")
+		resp.WriteError(w, err)
+		return
+	}
+
+	resp.WriteJSON(w, http.StatusOK, categories)
+}
+
+func (h *httpHandler) GetCategory(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		log.Ctx(ctx).Error().Err(err).Msg("invalid request")
+		resp.WriteJSON(w, http.StatusBadRequest, map[string]string{"message": "invalid request"})
+		return
+	}
+
+	category, err := h.service.FindByID(ctx, id)
+	if err != nil {
+		if err == ErrCategoryNotFound {
+			log.Ctx(ctx).Error().Err(err).Msg("category not found")
+			resp.WriteJSON(w, http.StatusNotFound, map[string]string{"message": "category not found"})
+			return
+		}
+
+		log.Ctx(ctx).Error().Err(err).Msg("failed to get category")
+		resp.WriteError(w, err)
+		return
+	}
+
+	resp.WriteJSON(w, http.StatusOK, category)
+}
+
+func (h *httpHandler) UpdateCategory(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		log.Ctx(ctx).Error().Err(err).Msg("invalid request")
+		resp.WriteJSON(w, http.StatusBadRequest, map[string]string{"message": "invalid request"})
+		return
+	}
+
+	var req UpdateCategoryRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		resp.WriteJSON(w, http.StatusBadRequest, map[string]string{"message": "invalid request"})
+		return
+	}
+
+	category := &entity.Category{
+		ID:   id,
+		Name: req.Name,
+	}
+
+	if err := h.service.Update(ctx, category); err != nil {
+		if err == ErrCategoryNotFound {
+			resp.WriteJSON(w, http.StatusNotFound, map[string]string{"message": "category not found"})
+			return
+		}
+
+		log.Ctx(ctx).Error().Err(err).Msg("failed to update category")
+		resp.WriteError(w, err)
+		return
+	}
+
+	resp.WriteJSON(w, http.StatusOK, map[string]string{"message": "category updated"})
+}
+
+func (h *httpHandler) DeleteCategory(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		log.Ctx(ctx).Error().Err(err).Msg("invalid request")
+		resp.WriteJSON(w, http.StatusBadRequest, map[string]string{"message": "invalid request"})
+		return
+	}
+
+	if err := h.service.Delete(ctx, id); err != nil {
+		if err == ErrCategoryNotFound {
+			resp.WriteJSON(w, http.StatusNotFound, map[string]string{"message": "category not found"})
+			return
+		}
+
+		log.Ctx(ctx).Error().Err(err).Msg("failed to delete category")
+		resp.WriteError(w, err)
+		return
+	}
+
+	resp.WriteJSON(w, http.StatusOK, map[string]string{"message": "category deleted"})
 }
